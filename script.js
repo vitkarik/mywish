638dcf1438c6b3daabe16ec4cfde56dc9bff53fb
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2022-09-19 14:02:36 +0200

    created new branch with chanched markup and elements'position

---
+++ script.js
@@ -0,0 +1,22 @@
+    let countDownDate = new Date("Sep 26, 2022 00:00:00").getTime();
+    let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    if (distance === 0 || distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdownfunc").innerHTML = `has expired`;
+        document.getElementById("countdownexpired").innerHTML = ":(";
+        document.getElementById("clear").innerHTML = "";
+      } else {
+        document.getElementById("clear").innerHTML = "is";
+        document.getElementById("countdownfunc").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      }
+    }, 1000);
+    
+
+    
